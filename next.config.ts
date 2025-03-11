7053f5293be24f7bce98df59fe7cc1bbabd5a923
OvanessTea <OvanessTea@yandex.com>
2025-03-11 02:30:01 +0100

    update configs by env vars

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY,
+    PRIVATE_KEY: process.env.PRIVATE_KEY,
+    CONTRACT_ADDRESS: process.env.CONTRACT_ADDRESS,
+    ALCHEMY_API_WEBSOCKET: process.env.ALCHEMY_API_WEBSOCKET,
+  },
 };
 
 export default nextConfig;
